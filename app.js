8e2786adba437a094712e741ba8d333bcf78c04d
DEV HARSH <harsh@Harshs-Air-2.mshome.net>
2024-02-23 22:26:57 +0530

    first commit

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
+require('dotenv').config()
+
 const port = 3000;
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+app.use('/users', users)
+app.use('/news', news)
+
 
 app.listen(port, (err) => {
     if (err) {
